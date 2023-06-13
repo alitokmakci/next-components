7786f7dcae66b56f65b4e823a9612cae2cf897c9
Ali Tokmakcı <ali@outlook.com>
2023-06-13 22:12:14 +0300

    Subtitle added

---
+++ @ui/components/Typography/Subtitle/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./index.module.css";
+import classNames from "classnames";
+import { SubtitleProps } from "./type";
+
+export default function Subtitle({
+  children,
+  className,
+  color = "text",
+  ...props
+}: SubtitleProps) {
+  return (
+    <p
+      {...props}
+      className={classNames(className, styles.subtitle, styles[color])}
+    >
+      {children}
+    </p>
+  );
+}
